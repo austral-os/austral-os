448dcfb9b1d23a4c2b0ddfec9917f220df4e67a3
Horacio Daniel Ros <auxtral.os@gmail.com>
2026-05-18 15:38:35 -0300

    feat: add horizon-set-compositor utility and greeter configuration for dynamic compositor switching

---
+++ config/live-build/config/includes.chroot/usr/bin/horizon-greeter-monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Esperar a que el greeter se inicie
+sleep 1
+
+# Monitorear el proceso del greeter perteneciente al usuario 'greeter'
+while pgrep -u greeter horizon-greeter >/dev/null; do
+    sleep 0.2
+done
+
+# El greeter ha finalizado, terminar el compositor de forma limpia para el usuario greeter
+pkill -u greeter wayfire
